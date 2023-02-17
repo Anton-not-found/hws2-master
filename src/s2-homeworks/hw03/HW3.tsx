3f8fcfe947d245ad12fe071e16698e165c6a3a15
Anton <shurmanov_anton@rambler.ru>
2023-02-17 21:26:12 +0300

    fix const user

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -26,10 +26,11 @@ export type UserType = {
 export const pureAddUserCallback = (name: string, setUsers: (users: Array<UserType>) => void, users: Array<UserType>) => { // need to fix any
     const user = {
         // need to fix
-        _id:v1(),
-        name: 'name'
+        _id: v1(),
+        name: name
     }
     setUsers([...users, user])
+    debugger
 }
 
 const HW3 = () => {
