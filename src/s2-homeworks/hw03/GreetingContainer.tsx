a0c040a00bbe7f0a5785294e40c03f3e9cd0fa74
Anton <shurmanov_anton@rambler.ru>
2023-02-18 17:43:21 +0300

    task HW3 is completed

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -3,8 +3,6 @@ import Greeting from './Greeting'
 import {UserType} from './HW3'
 
 
-
-
 type GreetingContainerPropsType = {
     users: Array<UserType> // need to fix any
     addUserCallback: (name: string) => void // need to fix any
@@ -25,11 +23,12 @@ export const pureOnBlur = (name: string, setError: (error: string) => void) => {
     if (name === '') {
         setError('Ошибка! Введите имя!')
     }
+
 }
 
-export const pureOnEnter = (e: KeyboardEvent<HTMLImageElement>, addUser: ()=>void) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => { // если нажата кнопка Enter - добавить
 
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
         addUser()
     }
 }
@@ -46,12 +45,12 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [name, setName] = useState<string>('') // need to fix any
     const [error, setError] = useState<string>('') // need to fix any
 
-    console.log(name)
-    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
 
-        setName(e.currentTarget.value ) // need to fix
+        setName(e.currentTarget.value) // need to fix
 
         error && setError('')
+
     }
 
 
@@ -62,15 +61,16 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
 
     const onBlur = () => {
         pureOnBlur(name, setError)
+
     }
 
-    const onEnter = (e: KeyboardEvent<HTMLImageElement>) => {
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
         pureOnEnter(e, addUser)
 
     }
 
     const totalUsers = users.length  // need to fix
-    const lastUserName = users.length ? users[users.length-1].name: '' // need to fix
+    const lastUserName = users.length ? users[users.length - 1].name : '' // need to fix
 
 
     return (
