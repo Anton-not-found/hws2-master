fb2a59ff97b0a605852324ebb70603a0c648ba1c
Anton <shurmanov_anton@rambler.ru>
2023-02-18 19:45:31 +0300

    fixed errors

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -20,7 +20,7 @@ export const pureAddUser = (name: string, setError: (error: string) => void, set
 }
 
 export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
-    if (name === '') {
+    if (name.trim() === '') {
         setError('Ошибка! Введите имя!')
     }
 
