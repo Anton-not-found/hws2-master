4afe0c3aff53d4b7aedefa605176f2d9c88c3518
Anton <shurmanov_anton@rambler.ru>
2023-03-25 16:39:55 +0300

    HW8 has been completed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,30 @@
-import {UserType} from '../HW8'
+
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+type StateType = {
+    _id: number
+    name: string
+    age: number
+}
+
+export const homeWorkReducer = (state: StateType[], action: ActionType): StateType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return [...state].sort((a, b) => action.payload === 'up' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))
+            // need to fix
         }
         case 'check': {
+            return state.filter(el => el.age >= 18)
 
-            return state // need to fix
+            // need to fix
         }
         default:
             return state
     }
 }
+
+
+
