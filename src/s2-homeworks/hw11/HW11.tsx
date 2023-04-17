9d63f30d23988346f4a0274e9bf30f373158b16b
Anton <shurmanov_anton@rambler.ru>
2023-04-17 19:45:58 +0300

    HW11 slider second has been fixed

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -18,7 +18,7 @@ function HW11() {
     // console.log('value1: ' + value1)
     // console.log('value2: ' + value2)
 
-    const change = (event:  React.SyntheticEvent | Event, value: number | number[]) => {
+    const change = (event: React.SyntheticEvent | Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
         // console.log('value:  ' + value)
         if (Array.isArray(value)) {
@@ -51,7 +51,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            value={[value1, value2]}
+                            onChange={change}
 
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
