ecc4c9c903ade4daf63e232393c1e7750eb671e1
Anton <shurmanov_anton@rambler.ru>
2023-04-23 14:01:06 +0300

    HW12 has been fixed

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -24,7 +24,7 @@ const HW12 = () => {
     // взять ид темы из редакса
     const themeId = useSelector<AppStoreType>(state => state.theme.themeId)
 
-    const change = (id: any) => { // дописать функцию
+    const change = (id: number) => { // дописать функцию
         dispatch(changeThemeId(id))
     }
 
